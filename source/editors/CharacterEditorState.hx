525753552b3fc90a9f8b707fbbd78237124442b8
tchcfhvgd <99186628+tchcfhvgd@users.noreply.github.com>
2024-10-26 10:43:30 +0800

    Update CharacterEditorState.hx

--- source/editors/CharacterEditorState.hx
+++ source/editors/CharacterEditorState.hx
@@ -28,7 +28,7 @@ import openfl.net.FileReference;
 import openfl.events.Event;
 import openfl.events.IOErrorEvent;
 import haxe.Json;
-import Character;
+import objects.Character;
 import flixel.system.debug.interaction.tools.Pointer.GraphicCursorCross;
 import lime.system.Clipboard;
 import flixel.animation.FlxAnimation;
