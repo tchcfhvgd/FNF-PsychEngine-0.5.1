81088091253454e3240c48f97d791b6a9fd17369
tchcfhvgd <99186628+tchcfhvgd@users.noreply.github.com>
2024-10-26 10:48:22 +0800

    Update Character.hx

--- source/objects/Character.hx
+++ source/objects/Character.hx
@@ -6,7 +6,7 @@ import flixel.animation.FlxBaseAnimation;
 import flixel.graphics.frames.FlxAtlasFrames;
 import flixel.tweens.FlxTween;
 import flixel.util.FlxSort;
-import Section.SwagSection;
+import backend.Section.SwagSection;
 #if MODS_ALLOWED
 import sys.io.File;
 import sys.FileSystem;
