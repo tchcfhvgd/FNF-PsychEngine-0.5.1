f1e62f4887d12b06066b1d35ce577f57c279fd18
tchcfhvgd <99186628+tchcfhvgd@users.noreply.github.com>
2024-10-26 08:48:53 +0800

    Update import.hx

--- source/import.hx
+++ source/import.hx
@@ -1,1 +1,21 @@
-import Paths;
+#if !macro
+import backend.*;
+
+//Flixel
+import flixel.sound.FlxSound;
+import flixel.FlxG;
+import flixel.FlxSprite;
+import flixel.FlxCamera;
+import flixel.util.FlxDestroyUtil;
+import flixel.math.FlxMath;
+import flixel.math.FlxPoint;
+import flixel.util.FlxColor;
+import flixel.util.FlxTimer;
+import flixel.text.FlxText;
+import flixel.tweens.FlxEase;
+import flixel.tweens.FlxTween;
+import flixel.group.FlxSpriteGroup;
+import flixel.group.FlxGroup.FlxTypedGroup;
+
+using StringTools;
+#end
