f437f3c279ad8480620136ce72dd5007d2c26c21
tchcfhvgd <99186628+tchcfhvgd@users.noreply.github.com>
2024-10-26 10:57:07 +0800

    Update MainMenuState.hx

--- source/states/MainMenuState.hx
+++ source/states/MainMenuState.hx
@@ -17,7 +17,7 @@ import flixel.tweens.FlxEase;
 import flixel.tweens.FlxTween;
 import flixel.util.FlxColor;
 import lime.app.Application;
-import Achievements;
+import backend Achievements;
 import editors.MasterEditorMenu;
 import flixel.input.keyboard.FlxKey;
 
